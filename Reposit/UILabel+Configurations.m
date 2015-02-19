ddcec7c21cfc0d56298b75c4e484e7ff1c11b026
Morgan Chen <morganchen12@gmail.com>
2015-02-19 13:18:57 -0800

    move more view logic out of controllers

---
+++ Reposit/UILabel+Configurations.m
@@ -0,0 +1,13 @@
+//
+//  UILabel+Configurations.m
+//  Reposit
+//
+//  Created by Morgan Chen on 2/19/15.
+//  Copyright (c) 2015 Morgan Chen. All rights reserved.
+//
+
+#import "UILabel+Configurations.h"
+
+@implementation UILabel_Configurations
+
+@end
